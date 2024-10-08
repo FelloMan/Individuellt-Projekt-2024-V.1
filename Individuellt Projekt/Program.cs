89a68b659de4b6c5e4bb30ae5b8e403fb35aec7f
FelloMan <filip0816@hotmail.com>
2024-10-08 10:54:07 +0200

    Add project files.

---
+++ Individuellt Projekt/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+
+namespace Individuellt_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] accounts;
+            double[] Newaccounts = new double[3];
+
+            string[,] users = new string[5, 3];
+            int index = 0;
+
+
+
+
+            users[0, 0] = "Filip Oldin";
+            users[0, 1] = "hemlis123";
+            users[0, 2] = "24";
+
+            users[1, 0] = "Anna Holgersson";
+            users[1, 1] = "hemlis1234";
+            users[1, 2] = "54";
+
+            users[2, 0] = "Tobbe Rikardsson";
+            users[2, 1] = "hemlis12345";
+            users[2, 2] = "43";
+
+            users[3, 0] = "Kent Käll";
+            users[3, 1] = "hemlis123456";
+            users[3, 2] = "32";
+
+            users[4, 0] = "Eva Hobert";
+            users[4, 1] = "hemlis1234567";
+            users[4, 2] = "19";
+
+            string userName = "";
+
+            int failedLoginAttempts = 0;
+            const int loginAttempts = 3;
+            bool isLoggedIn = false;
+
+
+            //Fixa login 
+            //Fixa moneytransfer
+
+
+            while (failedLoginAttempts < loginAttempts)
+            {
+
+                Console.WriteLine("Inlogningsmeny");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Skriv in ett ditt användarnam");
+                userName = Console.ReadLine();
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Skriv in ett ditt lösenord");
+                string password = Console.ReadLine();
+
+
+                if (checkLogin(userName, password))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Du är inloggad!");
+                    Console.WriteLine("Hej och välkommen till din kontomeny");
+                    failedLoginAttempts = 0;
+                    isLoggedIn = true;
+                    
+                }
+                else
+                {
+                    failedLoginAttempts++;
+                    Console.WriteLine($"Fel inloggning. Försök kvar: {loginAttempts - failedLoginAttempts}");
+                }
+
+
+                if (isLoggedIn)
+                {
+                    Console.WriteLine("Välkommen till huvudmenyn");
+
+                }
+                else
+                {
+                    Console.WriteLine("För många fel! Programmet stängs!");
+
+                }
+
+
+                accounts = getaccount(userName);
+                bool since = false;
+                while (since)
+                {
+
+                    int mainAccount;
+                    Console.WriteLine("");
+                    Console.WriteLine(" Skriv in en siffra till den del av kontot du vill till ");
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("1. Se dina konton och saldo ");
+                    Console.WriteLine("2. Överföring mellan konton ");
+                    Console.WriteLine("3. Ta ut pengar ");
+                    Console.WriteLine("4. Logga ut ");
+                    Console.WriteLine();
+
+
+                    mainAccount = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    // int num = int.Parse(Console.ReadLine()); //skriv alternativ
+                    switch (mainAccount)
+
+                    {
+
+
+                        case 1:
+
+                            Console.WriteLine("Saldo på lönekonto:");
+                            Console.WriteLine(accounts[0]);
+                            Console.WriteLine("Saldo på sparkonto: ");
+                            Console.WriteLine(accounts[1]);
+                            Console.WriteLine("Saldo på semesterkonto: ");
+                            Console.WriteLine(accounts[2]);
+                            Console.WriteLine("");
+                            Console.WriteLine(" Tryck på enter för att återgå till huvudmenyn");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Överföring mellan konton");
+                            Console.WriteLine("Välj ett konto att överföra från (1-3):");
+                            int fromUserAccount = int.Parse(Console.ReadLine()) - 1;
+                            Console.WriteLine("Välj ett konto att överföra till (1-3):");
+                            int toUserAccount = int.Parse(Console.ReadLine()) - 1;
+                            Console.WriteLine("Ange ett belopp att överföra:");
+
+
+                            if (decimal.TryParse(Console.ReadLine(), out decimal transferAmount))
+                            {
+                                TransferMoney(fromUserAccount, toUserAccount, transferAmount, accounts);
+
+                                Console.WriteLine("\nNytt Saldo");
+                                for (int i = 0; i < accounts.Length; i++)
+                                {
+                                    Console.WriteLine($"Konto {i + 1}: {accounts[i]} kr");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ogiltigt belopp angivet.");
+                            }
+                            Console.WriteLine("\nTryck enter för att återgå till huvudmenyn");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Ta ut pengar");
+                            accounts[0] = accounts[0] - 1 - 10000;
+                            accounts[1] = accounts[1] + 1 - 10000;
+                            accounts[0] = accounts[0] - 1 - 1000;
+                            accounts[1] = accounts[1] + 1000;
+                            Console.WriteLine("Skriv hur mycket du skulle vilja ta ut:");
+                            string accounts1 = Console.ReadLine();
+                            Console.WriteLine("");
+                            Console.WriteLine(" Tryck på enter för att återgå till huvudmenyn");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Logga ut");
+                            return;
+                            break;
+
+                        default:
+                            Console.WriteLine("Ogiltig svar, försök igen");
+                            since = false;
+                            continue;
+
+                    }
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+
+            }
+        }
+
+        private static void TransferMoney(int fromUserAccount, int toUserAccount, decimal transferAmount, double[] accounts)
+        {
+            throw new NotImplementedException();
+        }
+
+        static double[] getaccount(string userName)
+        {
+            switch (userName)
+            {
+                case "Filip Oldin":
+                    return new double[] { 20000, 45000, 30000 };
+                    break;
+                case "Anna Holgersson":
+                    return new double[] { 100000, 50000, 240000 };
+                    break;
+                case "Tobbe Rikardsson":
+                    return new double[] { 60000, 130000, 300000 };
+                    break;
+                case "Kent Käll":
+                    return new double[] { 33000, 90000, 50000 };
+                    break;
+                case "Eva Hobert":
+                    return new double[] { 10000, 25000, 10000 };
+                    break;
+                default:
+                    return new double[] { };
+                    break;
+
+            }
+        }
+
+        static bool checkLogin(string userName, string password)
+        {
+
+            if (userName == "Filip Oldin" && password == "hemlis123")
+            {
+                return true;
+            }
+            else if (userName == "Anna Holgersson" && password == "hemlis1234")
+            {
+                return true;
+            }
+            else if (userName == "Tobbe Rikardsson" && password == "hemlis12345")
+            {
+                return true;
+            }
+            else if (userName == "Kent Käll" && password == "hemlis123456")
+            {
+                return true;
+            }
+            else if (userName == "Eva Hobert" && password == "hemlis1234567")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static void TransferMoney(int fromUserAccount, int toUserAccount, decimal amount, decimal[] accounts)
+        {
+            if (fromUserAccount >= 0 && fromUserAccount < accounts.Length &&
+                toUserAccount >= 0 && toUserAccount < accounts.Length)
+            {
+                if (accounts[fromUserAccount] >= amount)
+                {
+                    accounts[fromUserAccount] -= amount;
+                    accounts[toUserAccount] += amount;
+                    Console.WriteLine($"Överföring av {amount} kr från användarkonto {fromUserAccount + 1} till konto {toUserAccount + 1} lyckades");
+                }
+                else
+                {
+                    Console.WriteLine("Otillräckligt saldo för överföring");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ogiltiga kontonummer");
+            }
+        }
+    }
+        
+}
+
